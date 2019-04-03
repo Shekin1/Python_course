c5353480240385f0183af0634be48e7c8bed9c50
Daniel de la Puente <d.delapuente@outlook.com>
2019-04-03 22:15:54 +0200

    Lambdas

---
+++ funcionesLambda.py
@@ -0,0 +1,3 @@
+from sumaTodos import sumaTodos
+
+print(sumaTodos(3, lambda x: x**3))
--- sumaTodos.py
+++ sumaTodos.py
@@ -1,10 +1,16 @@
 def normal(x):
     return x
 
+# normal = lambda x: x
+
 def sumaTodos(limitTo, f):
     resultado = 0
     for i in range(0, limitTo+1):
         resultado += f(i)
     return resultado
 
-print(sumaTodos(5, normal))
+if __name__ == "__main__":      #para que solo lo ejecute desde aquí
+    print(sumaTodos(5, normal))
+else:
+    print(__name__)
+        
